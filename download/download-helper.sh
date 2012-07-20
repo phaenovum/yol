282f072bf9de56f8e0e793a2adb9f143f5bfeb48
Lars Moellendorf <lars@moellendorf.eu>
2012-07-20 16:20:25 +0200

    simple script to automate downloads

---
+++ download/download-helper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget http://yagarto.de/source/toolchain/expat-2.0.1.tar.gz
+wget http://yagarto.de/source/toolchain/gmp-5.0.4.tar.bz2
+wget http://yagarto.de/source/toolchain/mpfr-2.4.2.tar.bz2
+wget http://yagarto.de/source/toolchain/mpc-0.8.1.tar.gz
+wget http://yagarto.de/source/toolchain/binutils-2.22.tar.bz2
+wget http://yagarto.de/source/toolchain/newlib-1.20.0.tar.gz
+wget http://yagarto.de/source/toolchain/gcc-4.7.1.tar.bz2
+wget http://yagarto.de/source/toolchain/gdb-7.4.1.tar.bz2
+
